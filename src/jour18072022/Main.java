770180efb72bdd44412f4395156f9557d9dc8e2e
SALIM <saissi1701@gmail.com>
2022-07-19 09:31:20 +0100

    Exemple file

--- src/jour18072022/Main.java
+++ src/jour18072022/Main.java
@@ -1,26 +1,38 @@
 package jour18072022;
 
 public class Main {
-    public static void numbers(int i) throws NegativeException{
-        if (i>=0){
-            System.out.println("le nombre est positive");
-        }else {
-        throw new NegativeException(i);
+    public static int facto(int n){
+        int resultat;
+        resultat=n;
+        for (int i=n-1;i>=1;i--){
+            resultat=resultat*i;
+        }
+        return resultat;
+    }
+    public static int puissance(int nombre,int puissance){
+        if (puissance==1){
+            return nombre;
+        }else{
+            return nombre*puissance(nombre,puissance-1);
         }
     }
+    public static int somme(int N){
+        if(N==1){
+            return 1;
+        }else{
+            return N+somme(N-1);
+        }
+    }
+    public static int factor(int n){
+        if (n==0 || n==1){
+            return 1;
+        }else{
+            return n*factor(n-1);
+        }
+    }
+
     public static void main(String[] args) {
-        Filepersonne p1=new Filepersonne("Aissi","Salim");
-
-        p1.enfiler(new Filepersonne("Maaroufi","Karima"));
-        p1.enfiler(new Filepersonne("DIAKHATE","Sidy"));
-        Filepersonne.consulter(p1);
-        p1=Filepersonne.defiler(p1);
-        Filepersonne.consulter(p1);
-        p1=Filepersonne.defiler(p1);
-        Filepersonne.consulter(p1);
-        p1=Filepersonne.defiler(p1);
-        Filepersonne.consulter(p1);
-
+        System.out.println(Main.puissance(5,10));
 
     }
 }
