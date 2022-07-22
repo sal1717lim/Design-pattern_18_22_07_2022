7035e98e215e794e072e9994bd411e88087f4b32
benjamin <benjamin_richard@outlook.fr>
2022-07-22 16:13:31 +0200

    test 3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test simon");
         System.out.println("2eme test");
+        System.out.println("3eme test");
     }
 }
